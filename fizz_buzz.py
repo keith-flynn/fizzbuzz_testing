350400514a49147bc5d03938efe33c348716df18
Keith Flynn <flynn.keith@gmail.com>
2022-10-29 23:31:39 -0400

    added delightfully clean fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+# FizzBuzz
+
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
